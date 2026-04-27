35226ca2f01fc542b4b0c908813d14974cde8074
Harsh Aggarwal <harshaggarwal.img@gmail.com>
2026-04-27 13:17:49 +0530

    changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ import FeaturesSection from '@/components/FeaturesSection';
 import PackagesSection from '@/components/PricingSection';
 import TestimonialsSection from '@/components/TestimonialsSection';
 import ContactSection from '@/components/ContactSection';
-import FAQSection from '@/components/FAQSection';
+import FAQSection from '@/components/FaqSection';
 import Footer from '@/components/Footer';
 
 const Index = () => {
